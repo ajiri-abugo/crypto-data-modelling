10727bff2920a369658cfbe0a3d5da70c28c5b0a
Martha Imoh <marthafridayimoh@gmail.com>
2025-04-01 00:29:00 +0000

    Data streaming with kafka

--- coinmarketcap_etl.py
+++ coinmarketcap_etl.py
@@ -1,63 +1,42 @@
 #import reqiured modules
+from dotenv import load_dotenv
+from confluent_kafka import Producer
 import requests
 import pandas as pd
 import json
 import csv
 import os
-from dotenv import load_dotenv
 
-#load environment variables from .env file
-load_dotenv()
 
-#not applicable
-'''with open("credentials.txt", "r") as f:
-    api_key = f.read()'''
+def get_data():
+    #load environment variables from .env file
+    load_dotenv()
 
-api_key = os.getenv('API_KEY')
+    api_key = os.getenv("API_KEY")
 
-headers = {
-    'Accepts': 'application/json',
-    'X-CMC_PRO_API_KEY': api_key,
-}
+    headers = {
+        'Accepts': 'application/json',
+        'X-CMC_PRO_API_KEY': api_key,
+    }
 
-# API endpoint
-url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+    # API endpoint
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 
-csv_file = 'crypto_data.csv'
-
-# Open the CSV file for writing
-with open(csv_file, mode='w', newline='', encoding="utf-8") as file:
-    writer = csv.writer(file)
-
-    writer.writerow(['id', 'name', 'symbol', 'date_added', 'Price', 'volume_24h', 'volume_change_24h', 'max_supply', 'circulating_supply', 'total_supply', 'infinite_supply', 'cmc_rank', 'last_updated'])
-    
     # Making API request
     r = requests.get(url, headers=headers)
-    data = r.json
+    data = r.json()
     # print(data)
+    print("Data extracted successfully")
+    return data
 
-    if r.status_code == 200:
-        data = r.json()
-   
-        for currency in data['data']:
-            id = currency["id"]
-            name = currency["name"]
-            symbol = currency["symbol"]
-            date_added = currency["date_added"]
-            Price = currency['quote']['USD']['price']
-            volume_24h = currency['quote']['USD']['volume_24h']
-            volume_change_24h = currency['quote']['USD']['volume_change_24h']
-            max_supply = currency["max_supply"]
-            circulating_supply = currency["circulating_supply"]
-            total_supply = currency["total_supply"]
-            infinite_supply = currency["infinite_supply"]
-            cmc_rank = currency["cmc_rank"]
-            last_updated = currency["last_updated"]
+def stream_data(data):
+    conf = {"bootstrap.servers": "localhost:9092"}
 
-            writer.writerow([id, name, symbol, date_added, Price, volume_24h, volume_change_24h, max_supply, circulating_supply, total_supply, infinite_supply, cmc_rank, last_updated])
+    producer = Producer(conf)
 
-        print(f"Data has been successfull written to csv")
+    producer.produce(topic="crypto-data", value=json.dumps(data))
 
-    else:
-        print(f"Error: {r.status_code} - {r.text}")
-
+    producer.flush()
+    print("Data loaded into kafkaProducer successfully")
+data = get_data()
+stream_data(data)
